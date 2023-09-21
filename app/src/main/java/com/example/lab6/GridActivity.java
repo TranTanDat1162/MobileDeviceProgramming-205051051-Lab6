bd036ed2eae0ce1369ce64cf32102561b5a2f1a5
TranTanDat1162 <dattt11062002@gmail.com>
2023-09-21 14:51:14 +0700

    Third commit - My Android Lab 6

--- app/src/main/java/com/example/lab6/GridActivity.java
+++ app/src/main/java/com/example/lab6/GridActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 public class GridActivity extends AppCompatActivity {
-
-    int id;
     Button mybt;
     @SuppressLint("MissingInflatedId")
     @Override
